e0826ece836a5b8b5de5dc154e95d480e6c7543d
Kazi Ahmed <kaaziahmed@gmail.com>
2022-03-06 01:21:22 -0500

    AddProductForm functional -- needs auth logic and frontend validation

--- client/components/AddProductForm.js
+++ client/components/AddProductForm.js
@@ -3,57 +3,57 @@ import { connect } from 'react-redux';
 import { addNewProductThunk } from '../store/products';
 
 export class AddProductForm extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        name: "",
-        price: 0,
-        description: ""
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event) {
-      this.setState({
-        [event.target.name]: event.target.value,
-      });
-    }
-  
-    handleSubmit(event) {
-      event.preventDefault();
-      this.props.addNewProduct({ ...this.state });
-      this.setState({name: "", price: 0, description: ""})
-    }
-  
-    render() {
-      const { name, price, description} = this.state;
-      const { handleSubmit, handleChange } = this;
-  
-      return (
-        <div>
-          <h3> Add A New Product </h3>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Product Name: </label>
-            <input name="name" onChange={handleChange} value={name} placeholder ="required" required />
-  
-            <label htmlFor="price">Price: </label>
-            <input name="price" onChange={handleChange} value={price} placeholder ="required" required />
-  
-            <label htmlFor="description">Description: </label>
-            <input name="description" onChange={handleChange} value={description} />
-  
-  
-            <button className="button" type="submit">Add Product</button>
-          </form>
-        </div>
-      );
-    }
-  }
-  
-const mapDispatchToProps = (dispatch) => {
-    return {
-      addNewProduct: (newProduct) => dispatch(addNewProductThunk(newProduct, history)),
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      price: 0,
+      description: ""
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.addNewProduct({ ...this.state });
+    this.setState({ name: "", price: 0, description: "" })
+  }
+
+  render() {
+    const { name, price, description } = this.state;
+    const { handleSubmit, handleChange } = this;
+
+    return (
+      <div>
+        <h3> Add A New Product </h3>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Product Name: </label>
+          <input name="name" onChange={handleChange} value={name} placeholder="required" required />
+
+          <label htmlFor="price">Price: </label>
+          <input name="price" onChange={handleChange} value={price} placeholder="required" required />
+
+          <label htmlFor="description">Description: </label>
+          <input name="description" onChange={handleChange} value={description} />
+
+
+          <button className="button" type="submit">Add Product</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addNewProduct: (newProduct) => dispatch(addNewProductThunk(newProduct, history)),
+  };
 };
-  
+
 export default connect(null, mapDispatchToProps)(AddProductForm);
--- client/store/products.js
+++ client/store/products.js
@@ -23,21 +23,21 @@ export const addAProduct = (newProduct) => {
 // Thunk Creators - returns aysnc function that dispatches the action creator
 // fetches the list of campuses
 export const fetchProductsThunk = () => {
-    return async (dispatch) => {
-      const response = await axios.get("/api/products");
-      const products = response.data;
-      dispatch(setProducts(products));
-    };
+  return async (dispatch) => {
+    const response = await axios.get("/api/products");
+    const products = response.data;
+    dispatch(setProducts(products));
+  };
 };
 
 // adds a new Product [ admin ]
 export const addNewProductThunk = (newProduct) => {
   return async (dispatch) => {
     try {
-      const response = await axios.post('/api/products');
-      const newProduct = response.data; 
-      dispatch(addAProduct(newProduct));
-    }catch ( error ) {
+      const response = await axios.post('/api/products', newProduct);
+      const data = response.data;
+      dispatch(addAProduct(data));
+    } catch (error) {
       console.log(" !! Error from the addNewProduct Thunk !!", error)
     }
   }
@@ -46,12 +46,12 @@ export const addNewProductThunk = (newProduct) => {
 // Reducer
 const initialState = []
 export default function productsReducer(state = initialState, action) {
-    switch (action.type) {
-      case SET_PRODUCTS:
-        return action.products;
-      case ADD_A_Product: 
-        return [...state, action.newProduct]
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.products;
+    case ADD_A_Product:
+      return [...state, action.newProduct]
+    default:
+      return state;
+  }
 }
