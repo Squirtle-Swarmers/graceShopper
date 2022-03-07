8c0954ac9ebbe5fa8a1b0b09fddbbaa4842b108e
averinpassmore <averinpassmore@gmail.com>
2022-03-07 07:47:52 -0800

    api routes almost done. need to get magic methods working

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,33 +1,64 @@
 const router = require('express').Router()
-const { models: { OrderDetails, Order }} = require('../db')
+const { models: { OrderDetails, Order, Product }} = require('../db')
 module.exports = router
 
+// GET /api/orders for ALL orders (ADMIN)
 router.get('/', async (req, res, next) => {
   try {
-    const orders = await Order.findAll()
+    const orders = await Order.findAll({
+      include: {
+        model: Product,
+        attributes: ['name']
+      }
+    })
     res.json(orders);
   } catch (error) {
     next(error)
   }
 })
 
+// GET /api/orders/:orderId for SINGLE order 
 router.get('/:orderId', async (req, res, next) => {
   try {
-    const order = await Order.findByPk(req.params.orderId)
+    const order = await Order.findAll({
+      where: {id: req.params.orderId},
+      include: {
+        model: Product,
+        attributes: ['name']
+      }
+    })
+    console.log(Object.keys(Order.prototype))
     res.json(order);
-  }
-  catch (err) {
-    next(err);
-  }
-});
-
-router.get('/:orderId/details', async (req, res, next) => {
-  try {
-    const orderDetails = await OrderDetails.findAll({where: {orderId: req.params.orderId}})
-    res.json(orderDetails);
   } catch (error) {
     next(error)
   }
-})
+});
+
+// POST /api/orders/:orderId for starting order
+router.post('/:orderId', async (req, res, next) => {
+  try {
+    const order = await Order.create(req.body)
+    res.json(order);
+  } catch (error) {
+    next(error)
+  }
+});
+
+// PUT /api/orders/:orderId for modifying cart
+router.put('/:orderId', async (req, res, next) => {
+  try {
+    const order = await Order.findAll({
+      where: {id: req.params.orderId},
+      include: {
+        model: Product,
+        attributes: ['name']
+      }
+    })
+    await order.setProducts(req.body)
+    res.json(order);
+  } catch (err) {
+    next(err);
+  }
+});
 
 //consider doing price low - high after t1 is done
--- server/api/products.js
+++ server/api/products.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const { models: { Product }} = require('../db')
 module.exports = router
 
-// GET api/products
+// GET /api/products for ALL products
 router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll()
@@ -12,12 +12,32 @@ router.get('/', async (req, res, next) => {
   }
 })
 
-// GET api/products/productId
+// GET api/products/productId for SINGLE product
 router.get('/:productId', async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.productId)
     res.json(product);
-  }catch (err) {
+  } catch (err) {
+    next(err);
+  }
+});
+
+// POST api/products/productId for ADMIN privileges
+router.post('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body)
+    res.json(product);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// PUT api/products/productId for ADMIN privileges
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    res.json(await product.update(req.body));
+  } catch (err) {
     next(err);
   }
 });
