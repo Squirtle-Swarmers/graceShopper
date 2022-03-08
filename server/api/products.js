9e81779d3d6be24da342199b974fbfe047f2561c
Kazi Ahmed <kaaziahmed@gmail.com>
2022-03-08 10:51:03 -0500

    fixed bugs

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { models: { Product } } = require('../db')
+const { requireToken, isAdmin } = require('./gatekeepingMiddleware')
 
 // GET api/products
 router.get('/', async (req, res, next) => {
