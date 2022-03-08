9b529137c721b14507c0af8d6326c4856e449cd5
Kazi Ahmed <kaaziahmed@gmail.com>
2022-03-08 16:25:35 -0500

    fixed 0 Product cart bug

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -8,14 +8,32 @@ export function Cart(props) {
     props.setCart(props.auth.id)
   }, [])
   const cart = props.cart;
-  console.log("// [ Cart Component ] - props.cart", cart);
-  return (
-    <div>
-      <h1>cart should be here im bitch</h1>
-    </div>
-  )
+  const productsForOrder = props.cart.orders;
+  console.log("cart: ", cart);
+  if (Object.keys(cart).length === 0) {
+    return <p> CART IS EMPTY </p>
+  } else if (cart.orders.length === 0) {
+    return <p> There are no orders in your cart </p>
+  } else {
+    return (
+      <div>
+        <p> Here Are your Products: </p>
+        {cart.orders[0].products[0].name}
+      </div>
+    )
 
+
+    // return (
+    //   <div>
+    //     <h1>cart should be here im bitch</h1>
+    //     <div>
+    //       <h1> Items in Cart </h1>
+    //     </div>
+    //   </div>
+    // )
+  }
 }
+
 
 function mapState(state) {
   console.log("// logging state from mapState in AllProducts", state);
